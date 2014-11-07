743bcde0d96c3d0d94eedea794e44d8622ccebb6
scoj213 <scoj2_13@uni.worc.ac.uk>
2014-11-07 14:53:39 +0000

    text / storage

--- textEntryCode/www/js/index.js
+++ textEntryCode/www/js/index.js
@@ -8,9 +8,11 @@ $(document).on("pagecreate","#pageone",function(){
 function submitText() {
 	var text = $('#textinput').val();
 	alert(text);
+	storeValue("app", text);
 }
 
 function storeValue(key, value) {
+	console.log("storing data");
 	window.localStorage.setItem(
 		key,
 		value
